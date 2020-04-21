17e41b34ab4666b1df1049a3df4a588bc9a9fecb
Willqvist <willqvist@gmail.com>
2020-04-21 22:15:33 +0200

    Modified build.js to print out colored messages. - removed parser.js, file not used anymore.

--- build.js
+++ build.js
@@ -1,27 +1,16 @@
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) {
-   if (error) {
-      console.log(error.message);
-      return;
-  }
-  if (stderr) {
-      console.log(stderr);
-      return;
-  }
-  console.log(stdout); 
-}
-
+const spawn = require('child_process').spawn;
 var os = require('os');
+
 var runType = process.argv[2];
 if(!runType) {
     throw new Error("No run type given! ex. node build compile");
 }
 if (os.type() === 'Linux') {
-   exec("npm run "+runType+"-linux", puts); 
+   spawn('npm', ["run",runType+"-linux"], { stdio: 'inherit' });
 }
 else if (os.type() === 'Darwin') 
 throw new Error("Mac OS is not supported yet! Sorry: ");
 else if (os.type() === 'Windows_NT') 
-   exec("npm run "+runType+"-windows", puts);
+spawn('npm', ["run",runType+"-windows"], { stdio: 'inherit' });
 else
    throw new Error("Unsupported OS found: " + os.type());
